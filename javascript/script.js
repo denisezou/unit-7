608621c94d377a4de15021a3df7b152aa7d83bdd
DenZou <dzou@u.northwestern.edu>
2019-03-11 18:02:08 -0500

    push3

--- javascript/script.js
+++ javascript/script.js
@@ -12,7 +12,7 @@ firebase.initializeApp(config);
 
 // Assign the reference to the database to a variable named 'database'
 // var database = ...
-let database = firebase.database();
+let dataRef = firebase.database();
 
 //initial values
 let name = "";
@@ -21,9 +21,8 @@ let arrivaltime = "";
 let frequencymin = "";
 var minAway
 
-function initializethis() {
 
-$("#submit-button").on("click", function(){
+$("#submit-button").on("click", function(event){
 
 let name = $("#input-train").val().trim();
 console.log(name);
@@ -37,10 +36,30 @@ console.log(arrivaltime);
 let frequencymin = $("#input-freq").val().trim();
 console.log(frequencymin);
 
-$("#here").append("<tr><td scope='row'>" + name + "</td><td>" + destination + "</td><td>" + frequencymin + "</td><td>" + arrivaltime+ "</td><td>" + minAway+ "</td></tr>");
+// Code for the push
+dataRef.ref().push({
+
+    name: name,
+    destination: destination,
+    arrivaltime: arrivaltime,
+    frequencymin: frequencymin,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
 });
-}
+});
+
+dataRef.ref().on("child_added", function(childSnapshot){
+
+    //log what is coming out of snapshot
+console.log(childSnapshot.val().name);
+console.log(childSnapshot.val().destination);
+console.log(childSnapshot.val().arrivaltime);
+console.log(childSnapshot.val().frequencymin);
+console.log(childSnapshot.val().dateAdded);
 
 
-//initialize the function
-initializethis();
+$("#here").append("<tr><td scope='row'>" + childSnapshot.val().name + "</td><td>" + childSnapshot.val().destination + "</td><td>" + childSnapshot.val().frequencymin + "</td><td>" + childSnapshot.val().arrivaltime+ "</td><td>" + childSnapshot.val().minAway+ "</td></tr>");
+
+
+}, function(errorObject){
+    console.log("Errors: " + errorObject.code);
+});
